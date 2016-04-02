243d6a86305f4150adf34544f900413ab3c1f4d8
Steve Kellock <steve@kellock.ca>
2016-04-02 13:12:11 -0400

    Removes toString as Ramda already had it.  Lulz.

--- test/toStringTest.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava'
-import RS from '../lib/'
-
-test('toString', (t) => {
-  t.same(RS.toString(null), null)
-  t.same(RS.toString(undefined), undefined)
-  t.same(RS.toString(42), '42')
-  t.same(RS.toString('hi'), 'hi')
-})
