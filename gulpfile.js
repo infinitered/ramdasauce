57a1ec9e4f2b577a489fd1df8bba6034c9307b23
Steve Kellock <steve@kellock.ca>
2016-04-02 14:01:36 -0400

    Renamed 'dist' gulp task to 'build'.  Removed other crap.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,12 @@
 const gulp = require('gulp')
-const concat = require('gulp-concat')
 const babel = require('gulp-babel')
 const rollup = require('gulp-rollup')
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
-
-gulp.task('dist', () => {
+gulp.task('build', () => {
   return gulp.src('lib/index.js')
     .pipe(rollup({}))
     .pipe(babel({
       presets: ['es2015', 'stage-2']
     }))
     .pipe(gulp.dest('./dist'))
-});
+})
