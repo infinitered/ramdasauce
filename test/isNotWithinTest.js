1d7203b71184285cb08f4492fa6fac678c8d599d
Steve Kellock <steve@kellock.ca>
2016-04-03 12:54:11 -0400

    Adds isNotWithin().

---
+++ test/isNotWithinTest.js
@@ -0,0 +1,15 @@
+import test from 'ava'
+import RS from '../lib/'
+
+const is200s = RS.isNotWithin(200, 299)
+
+test('isNotWithin', (t) => {
+  t.false(is200s(200))
+  t.false(is200s(200.1))
+  t.false(is200s(299))
+  t.true(is200s(199))
+  t.true(is200s(299.1))
+  t.true(is200s(300))
+  t.true(is200s(null))
+  t.true(RS.isNotWithin(null, null, null))
+})
