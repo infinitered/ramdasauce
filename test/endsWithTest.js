f19be0bb3853ba44a704ef3ad5cfe1794aad60e6
Kevin VanGelder <kevin@infinite.red>
2016-06-16 13:37:38 -0700

    added tests

--- test/endsWithTest.js
+++ test/endsWithTest.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import RS from '../lib/ramdasauce'
 
-test('startsWith', (t) => {
+test('endsWith', (t) => {
   t.ok(RS.endsWith('i', 'hi'))
   t.ok(RS.endsWith('ello', 'hello'))
   t.notOk(RS.endsWith('.', 'hi'))
